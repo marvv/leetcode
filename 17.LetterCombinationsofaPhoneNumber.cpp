6ff9e0f325cafd7b100d42ae00f191c34eff83d9
marvv <marv0713@163.com>
2018-10-08 01:02:33 +0800

    17

--- 17.LetterCombinationsofaPhoneNumber.cpp
+++ 17.LetterCombinationsofaPhoneNumber.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-    void dfs(string& digits, string dict[], int level, string& out, vector<string>& res)
-    {
-        if (level == digits.size()) res.push_back(out);
-        else
-        {
-            string tmp = dict[digits[level] - '2'];
-            for (int i =0; i<tmp.size(); i++)
-            {
-                out.push_back(tmp[i]);
-                dfs(digits, dict, level+1, out, res);
-                out.pop_back();
-            }
-        }
+    vector<string> letterCombinations(string digits) {
+        vector<string> result;
+        if (digits.empty()) return result;
+        
+        string dict[] = {"abc", "def", "ghi", "jkl", "mno" , "pqrs", "tuv", "wxyz"};
+        f(digits, "", dict, result);
+        return result;
     }
     
-    vector<string> letterCombinations(string digits) {       
-        vector<string> res;
-        if (digits.empty()) return res;
-        string dict[] = {"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-
-        string out;
-        dfs(digits, dict, 0, out, res);
-        return res;
+    void f(string& digits, string current, string dict[], vector<string>& result) {
+        if (current.size() == digits.size()) {
+            result.push_back(current);
+            return;
+        }
+        int index = current.size();
+        string tmp = dict[digits[index] - '2'];
+        for (int i=0; i<tmp.size();i++) {
+            current.push_back(tmp[i]);
+            f(digits, current, dict, result);
+            current.pop_back();
+        }
     }
 };

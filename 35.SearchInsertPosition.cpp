63bba55771b7210d86c17cc16f947638044c17ff
marvv <marv0713@163.com>
2018-11-04 00:27:14 +0800

    move on

---
+++ 35.SearchInsertPosition.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int index =0;
+        for (;index<nums.size();index++) {
+            if (nums[index] >= target) {
+                break;
+            }
+        }
+        return index;
+    }
+};
